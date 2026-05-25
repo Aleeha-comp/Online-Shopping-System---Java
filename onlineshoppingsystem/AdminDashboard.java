1b886b34005dcb70302cd9e7f7d8fbf6f7867143
romeesazubairkazmi-dev <romeesazubairkazmi@gmail.com>
2026-05-26 01:37:24 +0500

    errors resolved

--- onlineshoppingsystem/AdminDashboard.java
+++ onlineshoppingsystem/AdminDashboard.java
@@ -139,6 +139,18 @@ public class AdminDashboard extends JFrame {
                     "Seller"
             });
         }
+    
+
+     // Admins
+        for (Admin admin : Main.getAdmins()) {
+
+            userModel.addRow(new Object[]{
+                    admin.getUserId(),
+                    admin.getName(),
+                    admin.getEmail(),
+                    "Admin"
+            });
+        }
     }
 
     // ================= REMOVE SHOP ================
